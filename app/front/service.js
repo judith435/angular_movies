b15d31d268669308f9726ddcb42d01f1bc0a430a
Yehudit Ilson <yehuditi@one1.co.il>
2017-12-26 12:37:53 +0200

    save...

--- app/front/service.js
+++ app/front/service.js
@@ -5,12 +5,29 @@ movieApp.service('movieService', function($http) {
 
     this.getDirectors = function (success) { 
         $http({
-            method : "GET",
             url : 'http://localhost:8080/joint/movies-with-pictures/back/api/api.php',
-            data: ajaxData,
-        }).then(success, error);
-        
+            method : "GET",
+            params: {ctrl: 'director'}
+        }).then(function (response) {
+            
+            // on success
+            $scope.people = response.data;
+            
+        }, function (response) {
+            
+            // on error
+            console.log(response.data,response.status);
+            
+        });
     }
+    // this.getDirectors = function (success) { 
+    //     $http({
+    //         url : 'http://localhost:8080/joint/movies-with-pictures/back/api/api.php',
+    //         method : "GET",
+    //         params: {ctrl: 'director'}
+    //     }).then(success, error);
+        
+    // }
 
     function error(response) {
         alert("Sorry Error occured in get directors");
